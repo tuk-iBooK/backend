0ad4faf34103d6aa094acfcf7644439063b07841
kimich1218 <ickim1218@gmail.com>
2024-02-15 19:52:20 +0900

    feat: 이야기 전체 조회 구현

--- ibook_backend/story/serializers.py
+++ ibook_backend/story/serializers.py
@@ -2,10 +2,10 @@ from story.models import Character, Story, Background, StoryContent
 from rest_framework import serializers
 
 
-class StorySerializer(serializers.ModelSerializer):
+class StoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
-        fields = ["title"]
+        fields = ["title", "image"]  # 조회 시 'title'과 'image' 필드 포함
 
 
 class CharacterSerializer(serializers.ModelSerializer):
