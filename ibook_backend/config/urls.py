9009793b280a981af0d77171e0b0b56f3701d4e8
조성현 <agllwy116@naver.com>
2024-04-14 14:46:45 +0900

    chore : UrlPatterns 가독성 좋게 코드 수정

--- ibook_backend/config/urls.py
+++ ibook_backend/config/urls.py
@@ -18,18 +18,25 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # 회원 관련 API
     path("api/auth/register/", RegisterAPIView.as_view()),
     path("api/auth/login/", AuthAPIView.as_view()),
     path("api/auth/me/", AuthAPIView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
+    
+    # 유저 프로필 & 만든 이야기 API
     path("api/user/profile/", UserProfileView.as_view()),
     path("api/user/story/", UserStoryAPIView.as_view()),
-    path("api/token/refresh/", TokenRefreshView.as_view()),
+    
+    # 이야기, 이야기 내용 조회 및 이야기 초기 설정 API
+    path("api/story/", StoryAPIView.as_view()),
+    path("api/story-content/", StoryContentAPIView.as_view()),
     path("api/story/register/character/", CharacterAPIView.as_view()),
     path("api/story/register/", StoryAPIView.as_view()),
-    path("api/story/", StoryAPIView.as_view()),
     path("api/story/register/background/", BackgroundAPIView.as_view()),
+    
+    # 이야기 생성 & 저장 및 그림 생성 & 저장 API
     path("api/story/register/chatgpt/", ChatgptAPIView.as_view()),
     path("api/story/register/chatgpt/image/", ChatgptImageAPIView.as_view()),
-    path("api/story-content/", StoryContentAPIView.as_view()),
     path("api/story/save_story/", SaveStoryAPIView.as_view()),
 ]
